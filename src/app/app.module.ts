bd3e5fc90fa3b7e5d1de972ca7d8925f7151f1b4
sousa-p <p.soousa7@gmail.com>
2024-01-07 17:22:13 -0300

    feat: projects structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/components/section-projects/section-projects.component.ts
+++ src/app/components/section-projects/section-projects.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-section-projects',
   templateUrl: './section-projects.component.html',
   styleUrls: ['./section-projects.component.scss'],
 })
-export class SectionProjectsComponent  implements OnInit {
+export class SectionProjectsComponent implements OnInit {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {
+    this.swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      slidesPerView: 'auto',
+      loop: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: true,
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: 3,
+        },
+        768: {
+          slidesPerView: 1,
+        },
+      },
+    });
+  }
 
-  ngOnInit() {}
-
+  ngAfterViewInit() {
+    this.swiper?.update();
+  }
 }
