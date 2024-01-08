b52bab3873e1ae04c99668dc8d979dffba67ad47
sousa-p <p.soousa7@gmail.com>
2024-01-08 12:51:10 -0300

    refactor: use Ts to show stacks

--- src/app/components/section-tech/section-tech.component.ts
+++ src/app/components/section-tech/section-tech.component.ts
@@ -5,10 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './section-tech.component.html',
   styleUrls: ['./section-tech.component.scss'],
 })
-export class SectionTechComponent  implements OnInit {
-
-  constructor() { }
+export class SectionTechComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
 
+  frontEndStacks: String[] = [
+    'TypeScript',
+    'Angular',
+    'Ionic',
+    'Vue.js',
+    'Flutter',
+    'Others...',
+  ];
+
+  backEndStacks: String[] = [
+    'PHP',
+    'Laravel',
+    'Lumen',
+    'MySQL',
+    'RESTfulAPI',
+    'Others...',
+  ];
+
+  tools: String[] = [
+    'Git',
+    'Github',
+    'Figma',
+    'Photoshop',
+    'Docker',
+    'Others...',
+  ];
 }
